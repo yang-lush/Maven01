36874ff1d8d2620dd1f19f365848cd8611cbc0fe
杨青葱 <1278545330@qq.com>
2021-09-28 17:43:25 +0800

    commit 1.0.0

---
+++ src/main/java/com/demo01/test01/HelloServlet.java
@@ -0,0 +1,31 @@
+package com.demo01.test01;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //响应的类型 html
+        resp.setContentType("text/html; charset=UTF-8");
+        //获取响应的输出流
+        PrintWriter out = resp.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Hello World</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>我是Java下的Hello World!</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
